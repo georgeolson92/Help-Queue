36815743fb04346e67b5900b7f6f6ff6d0ad9671
Caleb Stokka and George Olson <caleb@>
2016-08-22 10:14:55 -0700

    transition to ticket page upon save

--- app/components/ticket-tile.js
+++ app/components/ticket-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteTicket(ticket) {
+      if (confirm("Are you sure you want to close this question?")) {
+        this.sendAction('deleteTicket', ticket);
+      }
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ export default Ember.Route.extend({
     save(params) {
       var newTicket = this.store.createRecord('ticket', params);
       newTicket.save();
-      this.transitionTo('index');
+      this.transitionTo('ticket', newTicket.id);
     }
   }
 });
